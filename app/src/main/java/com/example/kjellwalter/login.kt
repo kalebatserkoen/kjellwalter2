d40d95e5ea4fe9f88e3b6793b616aeddf4c903b1
133379 <133379@hondsrugcollege.nl>
2024-02-28 13:44:52 +0100

    geprogameerd

--- app/src/main/java/com/example/kjellwalter/login.kt
+++ app/src/main/java/com/example/kjellwalter/login.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,32 +37,11 @@ class login : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v = inflater.inflate(R.layout.fragment_login, container, false)
-        v.findViewById<Button>(R.id.button_login).setOnClickListener {
-            val naam = v.findViewById<TextView>(R.id.gebruikersnaam_login).text
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
 
-
+        view.findViewById<Button>(R.id.button_login).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_login_to_homepage)
         }
-        return v
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment login.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            login().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return view
     }
 }
--- app/src/main/java/com/example/kjellwalter/signup.kt
+++ app/src/main/java/com/example/kjellwalter/signup.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,26 +36,13 @@ class signup : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        val view = inflater.inflate(R.layout.fragment_signup, container, false)
+
+        view.findViewById<Button>(R.id.button_signup).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_signup_to_homepage)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment signup.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            signup().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
